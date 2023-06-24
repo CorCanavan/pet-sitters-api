b811774a961c4a6b09c172cf22961bbd8fa9ec71
“Corinne <corinne.canavan@gmail.com>
2023-06-23 18:06:08 -0600

    Refactor if statement to ternary;add in code comments for clarity

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 app.use(express.json());
 
 app.set('port', process.env.PORT || 3000);
+
 app.locals.title = 'Pet Sitter\'s API';
+
 app.locals.pets = [
   { id: 1, name: 'Doc', type: 'cat', breed: 'Domestic Short Hair', age: 2, food: 'Royal Canin\'s Urinary SO', medicine: 'N/A', favoriteToy: 'twist tie', favoriteTreat: 'Greenies', notes: 'Loves playing fetch' },
   { id: 2, name: 'Baba', type: 'dog', breed: 'Mixed', age: 16, food: 'Nulo', medicine: 'Glucosamine 2x a day', favoriteToy: 'squeaky llama', favoriteTreat: 'bully sticks', notes: 'Does not get along with other dogs!' },
@@ -15,21 +17,21 @@ app.get('/', (request, response) => {
   response.send('Oh hey Pet Sitter');
 });
 
+// GET all pets
 app.get('/api/v1/pets', (request, response) => {
   const { pets } = app.locals;
   response.json({ pets });
 })
 
+// GET specific pet by id
 app.get('/api/v1/pets/:id', (request, response) => {
   const { id } = request.params;
   const pet = app.locals.pets.find(pet => pet.id === parseInt(id));
 
-  if (!pet) {
-    return response.sendStatus(404);
-  }
-  response.status(200).json(pet);
+  !pet ? response.sendStatus(404) : response.status(200).json(pet);
 })
 
+// POST
 app.post('/api/v1/pets', (request, response) => {
   const id = Date.now();
   const { name, type, breed, age, food, medicine, favoriteToy, favoriteTreat, notes } = request.body;
