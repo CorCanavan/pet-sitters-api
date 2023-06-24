37c692d4fd3c6cd0c6643aafd29fdad4862814cb
“Corinne <corinne.canavan@gmail.com>
2023-06-23 18:33:51 -0600

    Move sample data to data.js file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const pets = require('./data.js');
 
 app.use(express.json());
 
@@ -7,11 +8,7 @@ app.set('port', process.env.PORT || 3000);
 
 app.locals.title = 'Pet Sitter\'s API';
 
-app.locals.pets = [
-  { id: 1, name: 'Doc', type: 'cat', breed: 'Domestic Short Hair', age: 2, food: 'Royal Canin\'s Urinary SO', medicine: 'N/A', favoriteToy: 'twist tie', favoriteTreat: 'Greenies', notes: 'Loves playing fetch' },
-  { id: 2, name: 'Baba', type: 'dog', breed: 'Mixed', age: 16, food: 'Nulo', medicine: 'Glucosamine 2x a day', favoriteToy: 'squeaky llama', favoriteTreat: 'bully sticks', notes: 'Does not get along with other dogs!' },
-  { id: 3, name: 'Darling', type: 'cat', breed: 'Domestic Short Hair', age: 14, food: 'Royal Canin Indoor 7+', medicine: 'Methimazole 2x a day', favoriteToy: 'ribbon', favoriteTreat: 'wet food', notes: 'Shy - wait for her to approach you' }
-]
+app.locals.pets = pets;
 
 app.get('/', (request, response) => {
   response.send('Oh hey Pet Sitter');
@@ -26,7 +23,7 @@ app.get('/api/v1/pets', (request, response) => {
 // GET specific pet by id
 app.get('/api/v1/pets/:id', (request, response) => {
   const { id } = request.params;
-  const pet = app.locals.pets.find(pet => pet.id === parseInt(id));
+  const pet = pets.find(pet => pet.id === parseInt(id));
 
   !pet ? response.sendStatus(404) : response.status(200).json(pet);
 })
@@ -36,7 +33,7 @@ app.post('/api/v1/pets', (request, response) => {
   const id = Date.now();
   const { name, type, breed, age, food, medicine, favoriteToy, favoriteTreat, notes } = request.body;
 
-  app.locals.pets.push({ id, name, type, breed, age, food, medicine, favoriteToy, favoriteTreat, notes });
+  pets.push({ id, name, type, breed, age, food, medicine, favoriteToy, favoriteTreat, notes });
 
   response.status(201).json({ id, name, type, breed, age, food, medicine, favoriteToy, favoriteTreat, notes})
 })
